a166d8439af616a08cab848d9f1208dad0ad574f
Tarun Boddupalli <tarunbod@gmail.com>
2017-12-17 19:56:14 -0500

    fix verification.py, gitignore python stuff

--- verification.py
+++ verification.py
@@ -1,25 +1,20 @@
 import os
 from secrets import SystemRandom, token_hex, compare_digest
 from Crypto.Hash import SHA256
-from Crypto.Cipher import AES 
+from Crypto.Cipher import AES
 from Crypto import Random
 
-def storedPassword():
-  pwd1 = input("Create your first password")
-
-def compareStrings(tokenOne,tokenTwo):
-  if (compare_digest(tokenOne,tokenTwo) == True):
+def compareStrings(tokenOne, tokenTwo):
+  if compare_digest(tokenOne, tokenTwo):
     print ("True")
   else:
     print ("Verification error")
-    
-def hashFunction(password,):
-  with password
+
+
+def hashFunction(password):
   s = SHA256.new(password.encode('utf-16'))
   return s.digest()
-  
-storedPassword()
-pwd2 = input("Enter your password")
+
+storedPassword = input("Create your first password: ")
+pwd2 = input("Enter your password: ")
 compareStrings(hashFunction(pwd2), hashFunction(storedPassword))
-
-
