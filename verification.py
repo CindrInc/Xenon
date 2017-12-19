b9e681cddc52ce508a6f9f7dae3f8b777d42018f
bharddwaj <bharddwajvemulapalli@gmail.com>
2017-12-18 22:11:21 -0500

    added username

--- verification.py
+++ verification.py
@@ -4,6 +4,14 @@ from Crypto.Hash import SHA256
 from Crypto.Cipher import AES
 from Crypto import Random
 
+def compareStringsWithoutCryptography(username1, username2):
+  if(username1 == username2):
+    print ("True")
+    return True
+  else: 
+    print("False")
+    return False
+
 def compareStrings(tokenOne, tokenTwo):
   if compare_digest(tokenOne, tokenTwo):
     print ("True")
@@ -15,6 +23,8 @@ def hashFunction(password):
   s = SHA256.new(password.encode('utf-16'))
   return s.digest()
 
+storedUsername = input("Create your username")
 storedPassword = input("Create your first password: ")
+username = ("Enter your username")
 pwd2 = input("Enter your password: ")
 compareStrings(hashFunction(pwd2), hashFunction(storedPassword))
